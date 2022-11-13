e95d445ee8bf22614b61c6b4f1c187f83d7160fe
AugurwelL <115207876+AugurwelL@users.noreply.github.com>
2022-11-13 17:34:09 +0900

    Update gator.js

--- gator.js
+++ gator.js
@@ -1,4 +1,19 @@
-function buttonClick(){
-    let gator = document.getElementById("button")
-    gator.innerHTML =  "🐊";
+document.getElementById("btn0").onclick = function(){
+  document.getElementById("pics").src="aruoaka.png";
+}
+
+document.getElementById("btn0").onclick = function(){
+  document.getElementById("pics").src="https://github.com/AugurwelL/dig-day20/blob/main/tired_man_mono.png?raw=true";
+}
+
+document.getElementById("btn1").onclick = function(){
+  document.getElementById("pics").src="https://github.com/AugurwelL/dig-day20/blob/main/stance.jpg?raw=true";
+}
+
+document.getElementById("btn2").onclick = function(){
+  document.getElementById("pics").src="https://github.com/AugurwelL/dig-day20/blob/main/1.jpg?raw=true";
+}
+
+document.getElementById("btn3").onclick = function(){
+  document.getElementById("pics").src="https://github.com/AugurwelL/dig-day20/blob/main/2.jpg?raw=true";
 }
